74c1d63877bb7cc2ef8baa9e97e0ddcd36d40c54
Harikrishnan <haripdm9446718962@gmail.com>
2024-01-30 07:19:01 +0530

    paginator not complete

--- project/Scripts/brokart/products/views.py
+++ project/Scripts/brokart/products/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
 def list_products(request):
+    page = 1
+    if  request.GET:
+        page = request.GET.get('page',1)
+
     product_list = Product.objects.all()
+    product_paginator = Paginator(product_list,3)
+    product_list = product_paginator.get_page(page)
     context = {'products':product_list}
     return render(request,'product_details.html',context)
 
