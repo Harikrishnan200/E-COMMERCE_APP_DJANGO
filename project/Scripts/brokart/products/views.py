5d9fefa5b08e00b0cf777cd8966bc5e9e2d0470e
Harikrishnan <haripdm9446718962@gmail.com>
2024-01-31 07:13:10 +0530

    latest product not completed

--- project/Scripts/brokart/products/views.py
+++ project/Scripts/brokart/products/views.py
@@ -5,14 +5,19 @@ import random
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    featured_product = Product.objects.order_by('priority')[:4]
+    latest_product = Product.objects.order_by('-id')[:4]
+    context = {'featured_product':featured_product,
+               'latest_product':latest_product
+               }
+    return render(request,'home.html',context)
 
 def list_products(request):
     page = 1
     if  request.GET:
         page = request.GET.get('page',1)
 
-    product_list = Product.objects.all()
+    product_list = Product.objects.order_by('-priority')
     product_paginator = Paginator(product_list,3)
     product_list = product_paginator.get_page(page)
     context = {'products':product_list}
